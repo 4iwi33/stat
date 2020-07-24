bd3869a8f3a8f82d1c00c11766d4bfa3343c0fa4
zayceva-nastya <59017854+zayceva-nastya@users.noreply.github.com>
2020-07-24 19:08:09 +0300

    stat

---
+++ public/index.php
@@ -0,0 +1,34 @@
+<?php
+include '../vendor/autoload.php';
+
+use App\Calculator\Calculator;
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form method="POST" action="?">
+        <input type="text" name="a">
+        <select name="op">
+            <option value="mul">*</option>
+            <option value="sum">+</option>
+        </select>
+        <input type="text" name="b">
+        <input type="submit" value="OK">
+    </form>
+</body>
+<?php
+if (!empty($_POST)) {
+    echo (new Calculator($_POST['a'], $_POST['b']))->{$_POST['op']}();
+}
+?>
+
+</html>
